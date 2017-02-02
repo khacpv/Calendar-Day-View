e426e85f9688489b32162809cce13b1a19af50dd
khacpham <khacpv@gmail.com>
2017-02-02 10:50:48 +0700

    update bintray

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Calendar Day View
 **via Jcenter**
 
 ````Gradle
-compile 'com.framgia.library.calendardayview:library:1.0.2.3'
+compile 'com.framgia.library.calendardayview:library:1.0.2.6'
 ````
 
 **or via JitPack (to get current code)**
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
+
+# Add configs
+# local.properties
+# - bintray.user=your_bintray_user_name
+# - bintray.apikey=your_bintray_api_key
+#
+# bintray url: https://bintray.com/khacpv/maven/Calendar_Day_View
+
 ./gradlew install
 ./gradlew bintrayUpload
+
+echo 'Upload to JCenter successfully!'
+echo 'Visit: https://bintray.com/khacpv/maven/Calendar_Day_View to update the newest version!'
